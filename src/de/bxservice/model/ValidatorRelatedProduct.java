1cdc61348477aae8e91c5fd8819aebeab437078f
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2017-12-31 14:16:51 +0100

    Code refactor

--- src/de/bxservice/model/ValidatorRelatedProduct.java
+++ src/de/bxservice/model/ValidatorRelatedProduct.java
@@ -26,6 +26,7 @@
 package de.bxservice.model;
 
 import java.math.BigDecimal;
+
 import org.adempiere.base.event.AbstractEventHandler;
 import org.adempiere.base.event.IEventTopics;
 import org.adempiere.exceptions.AdempiereException;
@@ -83,7 +84,7 @@ public class ValidatorRelatedProduct extends AbstractEventHandler{
 		}
 		else if (po instanceof MInvoiceLine && 
 				(type.equals(IEventTopics.PO_AFTER_NEW) || 
-						type.equals(IEventTopics.PO_AFTER_CHANGE) ) ) {
+						type.equals(IEventTopics.PO_AFTER_CHANGE))) {
 
 			createSupplementalInvoiceLines((MInvoiceLine)po, type);
 
@@ -106,17 +107,17 @@ public class ValidatorRelatedProduct extends AbstractEventHandler{
 	 * @param po
 	 */
 	private void allowDeletion(PO po) {
-		if(po instanceof MOrder)
+		if (po instanceof MOrder)
 			allowOrderDeletion((MOrder)po);
 
-		else if(po instanceof MInvoice)
+		else if (po instanceof MInvoice)
 			allowInvoiceDeletion((MInvoice)po);
 		
 	} //allowDeletion
 
 	private void allowInvoiceDeletion(MInvoice invoice) {
-		if(!invoice.isProcessed()){
-			for( MInvoiceLine line : invoice.getLines() ){
+		if (!invoice.isProcessed()) {
+			for (MInvoiceLine line : invoice.getLines()) {
 				line.set_ValueOfColumn("Bay_MasterInvoiceLine_ID", null);   //Allows delete when master is deleted
 				line.saveEx();
 			}
@@ -125,8 +126,8 @@ public class ValidatorRelatedProduct extends AbstractEventHandler{
 
 	private void allowOrderDeletion(MOrder order) {
 
-		if(!order.isProcessed()){
-			for( MOrderLine line : order.getLines() ){
+		if (!order.isProcessed()) {
+			for (MOrderLine line : order.getLines()) {
 				line.set_ValueOfColumn("Bay_MasterOrderLine_ID", null);   //Allows delete when master is deleted
 				line.saveEx();
 			}
@@ -137,17 +138,17 @@ public class ValidatorRelatedProduct extends AbstractEventHandler{
 	 * When the master product is changed, delete old related product lines
 	 * @param po
 	 */
-	private boolean removePreviousRelated(PO po){
+	private boolean removePreviousRelated(PO po) {
 		
-		if(po instanceof MOrderLine)
+		if (po instanceof MOrderLine)
 			return deleteSupplementaOrderLines((MOrderLine)po, true);
 
-		else if(po instanceof MInvoiceLine)
+		else if (po instanceof MInvoiceLine)
 			return deleteSupplementaInvoiceLines((MInvoiceLine)po, true);
 		
 		return false;
 		
-	}//removePreviousRelated
+	} //removePreviousRelated
 
 	/**
 	 * Delete related lines when the parent is deleted.
@@ -155,12 +156,11 @@ public class ValidatorRelatedProduct extends AbstractEventHandler{
 	 */
 	private void deleteSupplementalLines(PO po) {
 
-		if(po instanceof MOrderLine)
+		if (po instanceof MOrderLine)
 			deleteSupplementaOrderLines((MOrderLine)po, false);
 
-		else if(po instanceof MInvoiceLine)
+		else if (po instanceof MInvoiceLine)
 			deleteSupplementaInvoiceLines((MInvoiceLine)po, false);
-
 	
 	}//deleteSupplementalLines
 	
@@ -170,29 +170,28 @@ public class ValidatorRelatedProduct extends AbstractEventHandler{
 	 * @param isChanged
 	 * @return
 	 */
-	private boolean deleteSupplementaOrderLines(MOrderLine orderLine, boolean isChanged){
+	private boolean deleteSupplementaOrderLines(MOrderLine orderLine, boolean isChanged) {
 
 		MProduct product;
 		
 		//If the product is changed in the master line, delete old related product lines
-		if(isChanged && orderLine.is_ValueChanged(MOrderLine.COLUMNNAME_M_Product_ID)){
+		if (isChanged && orderLine.is_ValueChanged(MOrderLine.COLUMNNAME_M_Product_ID)) {
 			int previousProductId = (Integer) orderLine.get_ValueOld(MOrderLine.COLUMNNAME_M_Product_ID);
 			product = MProduct.get(Env.getCtx(), previousProductId);
-		}
-		else
+		} else
 			product = orderLine.getProduct();
 		
-		if ( product != null && hasRelatedProducts(product) ){
+		if (product != null && hasRelatedProducts(product)) {
 			
 			MOrder order = orderLine.getParent();
-			log.info("Deleting related lines for: "+product.getName() + " in order: " + order.get_ID());
+			log.info("Deleting related lines for: " + product.getName() + " in order: " + order.get_ID());
 
-			for( MOrderLine line : order.getLines() ){
-				if( line.get_Value("Bay_MasterOrderLine_ID")!=null && 
-						line.get_Value("Bay_MasterOrderLine_ID").equals(orderLine.get_ID()) ){
+			for (MOrderLine line : order.getLines()) {
+				if (line.get_Value("Bay_MasterOrderLine_ID") != null && 
+						line.get_Value("Bay_MasterOrderLine_ID").equals(orderLine.get_ID())) {
 					//If the change is made when the document is completed don't do anything
-					if( isChanged && !orderLine.is_ValueChanged(MOrderLine.COLUMNNAME_QtyEntered) &&
-							!orderLine.is_ValueChanged(MOrderLine.COLUMNNAME_M_Product_ID) )
+					if (isChanged && !orderLine.is_ValueChanged(MOrderLine.COLUMNNAME_QtyEntered) &&
+							!orderLine.is_ValueChanged(MOrderLine.COLUMNNAME_M_Product_ID))
 						return false;
 					line.set_ValueOfColumn("Bay_MasterOrderLine_ID", null);   //Allows delete when master is deleted
 					line.deleteEx(true, order.get_TrxName());
@@ -201,7 +200,7 @@ public class ValidatorRelatedProduct extends AbstractEventHandler{
 
 		}
 		return true;
-	}//deleteSupplementaOrderLines
+	} //deleteSupplementaOrderLines
 	
 	/**
 	 * Delete related invoice lines when a master product is deleted or modified
@@ -209,36 +208,34 @@ public class ValidatorRelatedProduct extends AbstractEventHandler{
 	 * @param isChanged
 	 * @return
 	 */
-	private boolean deleteSupplementaInvoiceLines(MInvoiceLine invoiceLine, boolean isChanged){
+	private boolean deleteSupplementaInvoiceLines(MInvoiceLine invoiceLine, boolean isChanged) {
 		MProduct product;
 		
-		if(isChanged && invoiceLine.is_ValueChanged(MOrderLine.COLUMNNAME_M_Product_ID)){
+		if (isChanged && invoiceLine.is_ValueChanged(MOrderLine.COLUMNNAME_M_Product_ID)) {
 			int previousProductId = (Integer) invoiceLine.get_ValueOld(MOrderLine.COLUMNNAME_M_Product_ID);
 			product = MProduct.get(Env.getCtx(), previousProductId);
-		}
-		else
+		} else
 			product = invoiceLine.getProduct();
 
-		if ( product != null && hasRelatedProducts(product) ){
+		if (product != null && hasRelatedProducts(product)) {
 			
 			MInvoice invoice = invoiceLine.getParent();
-			log.info("Creating related lines for: "+product.getName() + " in order: " + invoice.get_ID());
+			log.info("Creating related lines for: " + product.getName() + " in order: " + invoice.get_ID());
 			
-			for(MInvoiceLine line :invoice.getLines()){
-				if( line.get_Value("Bay_MasterInvoiceLine_ID")!=null && 
-						line.get_Value("Bay_MasterInvoiceLine_ID").equals(invoiceLine.get_ID()) ){
+			for (MInvoiceLine line :invoice.getLines()) {
+				if (line.get_Value("Bay_MasterInvoiceLine_ID") != null && 
+						line.get_Value("Bay_MasterInvoiceLine_ID").equals(invoiceLine.get_ID())) {
 					//If the change is made when the document is completed don't do anything
-					if( isChanged && !invoiceLine.is_ValueChanged(MOrderLine.COLUMNNAME_QtyEntered) &&
-							!invoiceLine.is_ValueChanged(MOrderLine.COLUMNNAME_M_Product_ID) )
+					if (isChanged && !invoiceLine.is_ValueChanged(MOrderLine.COLUMNNAME_QtyEntered) &&
+							!invoiceLine.is_ValueChanged(MOrderLine.COLUMNNAME_M_Product_ID))
 						return false;
 					line.set_ValueOfColumn("Bay_MasterInvoiceLine_ID", null); //Allows delete when master is deleted
 					line.deleteEx(true, invoice.get_TrxName());
 				}
 			}
-
 		}
 		return true;
-	}//deleteSupplementaInvoiceLines
+	} //deleteSupplementaInvoiceLines
 	
 	/**
 	 * Don't let the supplementary lines be deleted.
@@ -246,48 +243,47 @@ public class ValidatorRelatedProduct extends AbstractEventHandler{
 	 * @param po
 	 */
 	private void nonDeleteSupplementalLines(PO po) {
-		if( (po instanceof MOrderLine && po.get_Value("Bay_MasterOrderLine_ID") != null ) 
-				|| (po instanceof MInvoiceLine && po.get_Value("Bay_MasterInvoiceLine_ID") != null ) )
+		if ((po instanceof MOrderLine && po.get_Value("Bay_MasterOrderLine_ID") != null) 
+				|| (po instanceof MInvoiceLine && po.get_Value("Bay_MasterInvoiceLine_ID") != null))
 			throw new AdempiereException(Msg.getMsg(Env.getLanguage(Env.getCtx()), "BAY_SupplementalProducts"));
 		else 
 			deleteSupplementalLines(po);
 
-	}//nonDeletingSupplementalLines
+	} //nonDeletingSupplementalLines
 	
 	/**
 	 * Create new lines for related products
 	 * @param orderLine
 	 * @param type
 	 */
-	private void createSupplementalOrderLines(MOrderLine orderLine, String type){
+	private void createSupplementalOrderLines(MOrderLine orderLine, String type) {
 
 		MOrder order = orderLine.getParent();
 		MProduct product = orderLine.getProduct();
 
 		if (product != null && order.getC_POS_ID() == 0 && hasRelatedProducts(product)) {
 			try {
-				log.info("Creating related products for: "+product.getName() + " in order: " + order.get_ID());
+				log.info("Creating related products for: " + product.getName() + " in order: " + order.get_ID());
 
 				int lineNo = orderLine.getLine();
 
 				//If the record was modified delete previous supplementary lines to avoid duplicated
-				if (type.equals(IEventTopics.PO_AFTER_CHANGE)){
-					if( !removePreviousRelated(orderLine) )
+				if (type.equals(IEventTopics.PO_AFTER_CHANGE)) {
+					if(!removePreviousRelated(orderLine))
 						return;
 				}
 
-				for (MRelatedProduct related : MRelatedProduct.getRelatedLines(product))
-				{
+				for (MRelatedProduct related : MRelatedProduct.getRelatedLines(product)) {
 					// The conditional UOM works to check if the parent product has that UOM the related product is created. If it's blank it's always created.
 					int conditionalUOM = related.get_ValueAsInt("C_UOM_ID");
 
-					if( conditionalUOM == 0 || conditionalUOM == orderLine.getC_UOM_ID() ){
+					if (conditionalUOM == 0 || conditionalUOM == orderLine.getC_UOM_ID()) {
 
 						MOrderLine newLine = new MOrderLine(order);
 
 						newLine.setLine(++lineNo);
 						newLine.setM_Product_ID(related.getRelatedProduct_ID(), true);
-						if(related.get_ValueAsInt("Qty")!=0)
+						if (related.get_ValueAsInt("Qty") != 0)
 							newLine.setQty(BigDecimal.valueOf(related.get_ValueAsInt("Qty")).multiply(orderLine.getQtyEntered()));
 						else
 							newLine.setQty(BigDecimal.valueOf(1));
@@ -311,48 +307,47 @@ public class ValidatorRelatedProduct extends AbstractEventHandler{
 	 * @param invoiceLine
 	 * @param type
 	 */
-	private void createSupplementalInvoiceLines(MInvoiceLine invoiceLine, String type){
+	private void createSupplementalInvoiceLines(MInvoiceLine invoiceLine, String type) {
 
 		MInvoice invoice = invoiceLine.getParent();
 		MProduct product = invoiceLine.getProduct();
 
-		if ( product != null && hasRelatedProducts(product) && invoiceLine.getM_InOutLine_ID() == 0 ){
+		if (product != null && hasRelatedProducts(product) && invoiceLine.getM_InOutLine_ID() == 0) {
 			try {
 				log.info("Creating related products for: "+product.getName() + " in invoice: " + invoice.get_ID());
 
 				int lineNo = invoiceLine.getLine();
 
 				//If the record was modified delete previous supplementary lines to avoid duplicated
-				if (type.equals(IEventTopics.PO_AFTER_CHANGE)){
-						if( !removePreviousRelated(invoiceLine) )
+				if (type.equals(IEventTopics.PO_AFTER_CHANGE)) {
+						if (!removePreviousRelated(invoiceLine))
 							return;
 				}
 
-				for (MRelatedProduct related : MRelatedProduct.getRelatedLines(product))
-				{
+				for (MRelatedProduct related : MRelatedProduct.getRelatedLines(product)) {
 					// The conditional UOM works to check if the parent product has that UOM the related product is created. If it's blank it's always created.
 					int conditionalUOM = related.get_ValueAsInt("C_UOM_ID");
 
-					if( conditionalUOM == 0 || conditionalUOM == invoiceLine.getC_UOM_ID() ){
+					if (conditionalUOM == 0 || conditionalUOM == invoiceLine.getC_UOM_ID()) {
 
 						MInvoiceLine newLine = new MInvoiceLine(invoice);
 
 						newLine.setLine(++lineNo);
 						newLine.setM_Product_ID(related.getRelatedProduct_ID(), true);
-						if(related.get_ValueAsInt("Qty")!=0)
+						if (related.get_ValueAsInt("Qty") != 0)
 							newLine.setQty(BigDecimal.valueOf(related.get_ValueAsInt("Qty")).multiply(invoiceLine.getQtyEntered()));
 						else
-							newLine.setQty(BigDecimal.valueOf(1));						
+							newLine.setQty(BigDecimal.valueOf(1));
 						if (related.getDescription() != null)
 							newLine.setDescription(related.getDescription());
 						newLine.setPrice();
 						newLine.set_ValueOfColumn("Bay_MasterInvoiceLine_ID", invoiceLine.get_ID());
 						newLine.saveEx(invoice.get_TrxName());
 
-						log.info("A new invoice line was added with product: "+related.getRelatedProduct().getName());
+						log.info("A new invoice line was added with product: " + related.getRelatedProduct().getName());
 					}
 				}
-			}  catch (Exception e) {
+			} catch (Exception e) {
 				throw new AdempiereException("Error creating invoice line. Cause: " + e.getLocalizedMessage());
 			}
 		}
@@ -391,10 +386,9 @@ public class ValidatorRelatedProduct extends AbstractEventHandler{
 		}
 	} //createSupplementalInvoiceLines
 
-	private boolean hasRelatedProducts(MProduct product){
-		if(	MRelatedProduct.getRelatedLines(product) == null || MRelatedProduct.getRelatedLines(product).size()==0 )
+	private boolean hasRelatedProducts(MProduct product) {
+		if (MRelatedProduct.getRelatedLines(product) == null || MRelatedProduct.getRelatedLines(product).size()==0)
 			return false;
 		return true;
-	}//hasRelatedProducts
-
+	} //hasRelatedProducts
 }

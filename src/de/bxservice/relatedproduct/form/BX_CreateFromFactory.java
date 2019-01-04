45f3bd2df4e9ca4016513ec1a1c1369483fb3beb
Carlos Ruiz <carg67@gmail.com>
2019-01-04 19:50:51 +0100

    gforge [2954] - improve create lines from in the invoice window to show just the non-supplemental lines (the supplemental lines are created automatically)

---
+++ src/de/bxservice/relatedproduct/form/BX_CreateFromFactory.java
@@ -0,0 +1,45 @@
+/**********************************************************************
+* This file is part of iDempiere ERP Open Source                      *
+* http://www.idempiere.org                                            *
+*                                                                     *
+* Copyright (C) Contributors                                          *
+*                                                                     *
+* This program is free software; you can redistribute it and/or       *
+* modify it under the terms of the GNU General Public License         *
+* as published by the Free Software Foundation; either version 2      *
+* of the License, or (at your option) any later version.              *
+*                                                                     *
+* This program is distributed in the hope that it will be useful,     *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+* GNU General Public License for more details.                        *
+*                                                                     *
+* You should have received a copy of the GNU General Public License   *
+* along with this program; if not, write to the Free Software         *
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,          *
+* MA 02110-1301, USA.                                                 *
+*                                                                     *
+* Contributors:                                                       *
+* - Carlos Ruiz - globalqss                                           *
+**********************************************************************/
+
+package de.bxservice.relatedproduct.form;
+
+import org.compiere.grid.ICreateFrom;
+import org.compiere.grid.ICreateFromFactory;
+import org.compiere.model.GridTab;
+import org.compiere.model.I_C_Invoice;
+
+public class BX_CreateFromFactory implements ICreateFromFactory 
+{
+
+	@Override
+	public ICreateFrom create(GridTab mTab) 
+	{
+		String tableName = mTab.getTableName();
+		if (tableName.equals(I_C_Invoice.Table_Name))
+			return new BX_WCreateFromInvoiceUI(mTab);
+		return null;
+	}
+
+}
---
+++ src/de/bxservice/relatedproduct/form/BX_WCreateFromInvoiceUI.java
@@ -0,0 +1,145 @@
+/**********************************************************************
+ * This file is part of iDempiere ERP Open Source                      *
+ * http://www.idempiere.org                                            *
+ *                                                                     *
+ * Copyright (C) Contributors                                          *
+ *                                                                     *
+ * This program is free software; you can redistribute it and/or       *
+ * modify it under the terms of the GNU General Public License         *
+ * as published by the Free Software Foundation; either version 2      *
+ * of the License, or (at your option) any later version.              *
+ *                                                                     *
+ * This program is distributed in the hope that it will be useful,     *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        *
+ * GNU General Public License for more details.                        *
+ *                                                                     *
+ * You should have received a copy of the GNU General Public License   *
+ * along with this program; if not, write to the Free Software         *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,          *
+ * MA 02110-1301, USA.                                                 *
+ *                                                                     *
+ * Contributors:                                                       *
+ * - Carlos Ruiz - globalqss                                           *
+ **********************************************************************/
+
+package de.bxservice.relatedproduct.form;
+
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Vector;
+import java.util.logging.Level;
+
+import org.adempiere.webui.apps.form.WCreateFromInvoiceUI;
+import org.compiere.model.GridTab;
+import org.compiere.model.MOrder;
+import org.compiere.util.CLogger;
+import org.compiere.util.DB;
+import org.compiere.util.Env;
+import org.compiere.util.KeyNamePair;
+
+public class BX_WCreateFromInvoiceUI extends WCreateFromInvoiceUI {
+
+	/**	Logger			*/
+	private CLogger log = CLogger.getCLogger(getClass());
+
+	public BX_WCreateFromInvoiceUI(GridTab tab) {
+		super(tab);
+	}
+
+	/**
+	 *  Load Data - Order
+	 *  @param C_Order_ID Order
+	 *  @param forInvoice true if for invoice vs. delivery qty
+	 */
+	protected Vector<Vector<Object>> getOrderData (int C_Order_ID, boolean forInvoice)
+	{
+		/**
+		 *  Selected        - 0
+		 *  Qty             - 1
+		 *  C_UOM_ID        - 2
+		 *  M_Product_ID    - 3
+		 *  VendorProductNo - 4
+		 *  OrderLine       - 5
+		 *  ShipmentLine    - 6
+		 *  InvoiceLine     - 7
+		 */
+		if (log.isLoggable(Level.CONFIG)) log.config("C_Order_ID=" + C_Order_ID);
+		p_order = new MOrder (Env.getCtx(), C_Order_ID, null);
+
+		Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+		StringBuilder sql = new StringBuilder("SELECT "
+			+ "l.QtyOrdered-SUM(COALESCE(m.Qty,0)),"					//	1
+			+ "CASE WHEN l.QtyOrdered=0 THEN 0 ELSE l.QtyEntered/l.QtyOrdered END,"	//	2
+			+ " l.C_UOM_ID,COALESCE(uom.UOMSymbol,uom.Name),"			//	3..4
+			+ " COALESCE(l.M_Product_ID,0),COALESCE(p.Name,c.Name),po.VendorProductNo,"	//	5..7
+			+ " l.C_OrderLine_ID,l.Line "								//	8..9
+			+ "FROM C_OrderLine l"
+			+ " LEFT OUTER JOIN M_Product_PO po ON (l.M_Product_ID = po.M_Product_ID AND l.C_BPartner_ID = po.C_BPartner_ID) "
+			+ " LEFT OUTER JOIN M_MatchPO m ON (l.C_OrderLine_ID=m.C_OrderLine_ID AND ");
+		sql.append(forInvoice ? "m.C_InvoiceLine_ID" : "m.M_InOutLine_ID");
+		sql.append(" IS NOT NULL)")
+			.append(" LEFT OUTER JOIN M_Product p ON (l.M_Product_ID=p.M_Product_ID)"
+			+ " LEFT OUTER JOIN C_Charge c ON (l.C_Charge_ID=c.C_Charge_ID)");
+		if (Env.isBaseLanguage(Env.getCtx(), "C_UOM"))
+			sql.append(" LEFT OUTER JOIN C_UOM uom ON (l.C_UOM_ID=uom.C_UOM_ID)");
+		else
+			sql.append(" LEFT OUTER JOIN C_UOM_Trl uom ON (l.C_UOM_ID=uom.C_UOM_ID AND uom.AD_Language='")
+				.append(Env.getAD_Language(Env.getCtx())).append("')");
+		//
+		sql.append(" WHERE l.C_Order_ID=? "			//	#1
+
+			/***********************************************************************
+			 * BX Service!! - this is the only change for related product from core
+			 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv*/
+			+ " AND l.bay_masterorderline_id IS NULL "
+			/*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+
+			+ "GROUP BY l.QtyOrdered,CASE WHEN l.QtyOrdered=0 THEN 0 ELSE l.QtyEntered/l.QtyOrdered END, "
+			+ "l.C_UOM_ID,COALESCE(uom.UOMSymbol,uom.Name),po.VendorProductNo, "
+				+ "l.M_Product_ID,COALESCE(p.Name,c.Name), l.Line,l.C_OrderLine_ID "
+			+ "ORDER BY l.Line");
+		//
+		if (log.isLoggable(Level.FINER)) log.finer(sql.toString());
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try
+		{
+			pstmt = DB.prepareStatement(sql.toString(), null);
+			pstmt.setInt(1, C_Order_ID);
+			rs = pstmt.executeQuery();
+			while (rs.next())
+			{
+				Vector<Object> line = new Vector<Object>();
+				line.add(new Boolean(false));           //  0-Selection
+				BigDecimal qtyOrdered = rs.getBigDecimal(1);
+				BigDecimal multiplier = rs.getBigDecimal(2);
+				BigDecimal qtyEntered = qtyOrdered.multiply(multiplier);
+				line.add(qtyEntered);                   //  1-Qty
+				KeyNamePair pp = new KeyNamePair(rs.getInt(3), rs.getString(4).trim());
+				line.add(pp);                           //  2-UOM
+				pp = new KeyNamePair(rs.getInt(5), rs.getString(6));
+				line.add(pp);                           //  3-Product
+				line.add(rs.getString(7));				// 4-VendorProductNo
+				pp = new KeyNamePair(rs.getInt(8), rs.getString(9));
+				line.add(pp);                           //  5-OrderLine
+				line.add(null);                         //  6-Ship
+				line.add(null);                         //  7-Invoice
+				data.add(line);
+			}
+		}
+		catch (Exception e)
+		{
+			log.log(Level.SEVERE, sql.toString(), e);
+		}
+		finally
+		{
+			DB.close(rs, pstmt);
+			rs = null; pstmt = null;
+		}
+
+		return data;
+	}   //  LoadOrder
+
+}
